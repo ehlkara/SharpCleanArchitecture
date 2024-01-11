a1349c1a0abcb223619b02612519630d2db871b2
ehlkara <52125689+ehlkara@users.noreply.github.com>
2024-01-11 11:15:35 +0300

    deleted Project

--- HR.LeaveManagement.Clean/src/Core/HR.LeaveManagement.Domain/LeaveType.cs
+++
@@ -1,9 +0,0 @@
-﻿using HR.LeaveManagement.Domain.Common;
-
-namespace HR.LeaveManagement.Domain;
-public class LeaveType : BaseEntity
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public int DefaultDays { get; set; }
-}
