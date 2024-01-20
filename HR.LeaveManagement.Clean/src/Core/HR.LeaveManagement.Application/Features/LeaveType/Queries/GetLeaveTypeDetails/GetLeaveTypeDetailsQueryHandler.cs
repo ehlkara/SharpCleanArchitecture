c091a5099f79ec647aad9d965e91c20e9cb2dfcf
ehlkara <ehlullah.fb@gmail.com>
2024-01-20 13:24:27 +0300

    added getLeaveTypeDetails request and handler

---
+++ HR.LeaveManagement.Clean/src/Core/HR.LeaveManagement.Application/Features/LeaveType/Queries/GetLeaveTypeDetails/GetLeaveTypeDetailsQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
+
+public class GetLeaveTypeDetailsQueryHandler : IRequestHandler<GetLeaveTypeDetailsQuery, LeaveTypeDetailsDto>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+    public GetLeaveTypeDetailsQueryHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+    {
+        _mapper = mapper;
+        _leaveTypeRepository = leaveTypeRepository;
+    }
+
+    public async Task<LeaveTypeDetailsDto> Handle(GetLeaveTypeDetailsQuery request, CancellationToken cancellationToken)
+    {
+        // Query To Database
+        var leaveType = await _leaveTypeRepository.GetByIdAsync(request.Id);
+
+        // convert data objects to DTO objects
+        var data = _mapper.Map<LeaveTypeDetailsDto>(leaveType);
+
+        //return list of object
+        return data;
+    }
+}
+
--- HR.LeaveManagement.Clean/src/Core/HR.LeaveManagement.Application/obj/Debug/net8.0/HR.LeaveManagement.Application.AssemblyInfo.cs
+++ HR.LeaveManagement.Clean/src/Core/HR.LeaveManagement.Application/obj/Debug/net8.0/HR.LeaveManagement.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HR.LeaveManagement.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a70babac2c5c953f9bf3b299938ca0b7e464f6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c322efe77b5fd361716670bbd1ec691fbed64f28")]
 [assembly: System.Reflection.AssemblyProductAttribute("HR.LeaveManagement.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HR.LeaveManagement.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
