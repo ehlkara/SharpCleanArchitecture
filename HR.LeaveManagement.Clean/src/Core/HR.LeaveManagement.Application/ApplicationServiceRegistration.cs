8a70babac2c5c953f9bf3b299938ca0b7e464f6e
ehlkara <ehlullah.fb@gmail.com>
2024-01-20 12:50:41 +0300

    Setup features folder structure

---
+++ HR.LeaveManagement.Clean/src/Core/HR.LeaveManagement.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Application;
+
+public static class ApplicationServiceRegistration
+{
+	public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+	{
+		services.AddAutoMapper(Assembly.GetExecutingAssembly());
+		services.AddMediatR(Assembly.GetExecutingAssembly());
+
+		return services;
+	}
+}
+
