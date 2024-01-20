c322efe77b5fd361716670bbd1ec691fbed64f28
ehlkara <ehlullah.fb@gmail.com>
2024-01-20 13:05:07 +0300

    created first request and handler

--- HR.LeaveManagement.Clean/src/Core/HR.LeaveManagement.Application/obj/Debug/net8.0/HR.LeaveManagement.Application.AssemblyInfo.cs
+++ HR.LeaveManagement.Clean/src/Core/HR.LeaveManagement.Application/obj/Debug/net8.0/HR.LeaveManagement.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HR.LeaveManagement.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e05382a62fcc5bdd73e6fd3bed3df5d5c56e0de7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a70babac2c5c953f9bf3b299938ca0b7e464f6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HR.LeaveManagement.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HR.LeaveManagement.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
