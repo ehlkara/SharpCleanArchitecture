ff5cf7e9bbec729fc0d3389a18fb4abb6a659e1d
ehlkara <ehlullah.fb@gmail.com>
2024-02-02 11:40:51 +0300

    completed repository

--- HR.LeaveManagement.Clean/src/Core/HR.LeaveManagement.Domain/LeaveAllocation.cs
+++ HR.LeaveManagement.Clean/src/Core/HR.LeaveManagement.Domain/LeaveAllocation.cs
@@ -4,7 +4,9 @@ namespace HR.LeaveManagement.Domain;
 
 public class LeaveAllocation : BaseEntity
 {
+    public string EmployeeId { get; set; } = string.Empty;
     public int NumberOfDays { get; set; }
     public LeaveType? LeaveType { get; set; }
+    public int LeaveTypeId { get; set; }
     public int Period { get; set; }
 }
