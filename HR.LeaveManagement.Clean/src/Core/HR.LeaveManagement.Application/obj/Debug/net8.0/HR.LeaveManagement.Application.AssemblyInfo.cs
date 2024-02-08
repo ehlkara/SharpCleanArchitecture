ee825080af66ebb2a7eb60c09bc564e651e45cbf
ehlkara <ehlullah.fb@gmail.com>
2024-02-08 21:52:06 +0300

    created application unit project

--- HR.LeaveManagement.Clean/src/Core/HR.LeaveManagement.Application/obj/Debug/net8.0/HR.LeaveManagement.Application.AssemblyInfo.cs
+++ HR.LeaveManagement.Clean/src/Core/HR.LeaveManagement.Application/obj/Debug/net8.0/HR.LeaveManagement.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HR.LeaveManagement.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f30faab6ebd4f23a86fba355c1f5b6aee752dbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aae2c8749af415fe3de4bd29459be3cc5c2c068")]
 [assembly: System.Reflection.AssemblyProductAttribute("HR.LeaveManagement.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HR.LeaveManagement.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
